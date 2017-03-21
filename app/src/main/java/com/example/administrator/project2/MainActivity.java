5dfdf5322a5c8d5950bdc2fba1f7d442566cd9c2
linylx <1075389505@qq.com>
2017-03-21 19:44:50 +0800

    img

--- app/src/main/java/com/example/administrator/project2/MainActivity.java
+++ app/src/main/java/com/example/administrator/project2/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.tablelayout);
+        setContentView(R.layout.linearlayout);
     }
 }
